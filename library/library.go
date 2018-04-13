61eec36ff403f944e90bf52a392086649ae2036c
adam mcdaniel <adam.mcdaniel17@gmail.com>
2018-04-13 11:48:55 -0400

    Add files via upload

---
+++ library/library.go
@@ -0,0 +1,53 @@
+package library
+
+import (
+	. "../virtualmachine"
+  // "math"
+  // "fmt"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+var size_x float64
+var size_y float64
+var title string
+
+func Start() {
+	cfg := pixelgl.WindowConfig{
+		Title:  title,
+		Bounds: pixel.R(0, 0, size_x, size_y),
+		VSync:  true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.Clear(colornames.Skyblue)
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
+
+func MakeGame(vm *VirtualMachine) {
+	// fmt.Println("Making game...")
+	title = vm.Pop().(string)
+	size_x = vm.Pop().(float64)
+	size_y = vm.Pop().(float64)
+	pixelgl.Run(Start)
+}
+
+func GetLibrary() map[string]func(*VirtualMachine) {
+  library := make(map[string]func(*VirtualMachine))
+  library["game.make"] = MakeGame
+  return library
+}
+
+func InstallLibrary(vm *VirtualMachine) {
+  vm.Push("{ game.make % }")
+  vm.Push("make")
+  vm.Op_store(false)
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"./parser"
+	"./virtualmachine"
+	"./library"
+	"fmt"
+)
+
+func PrintList(tokens []string) {
+	fmt.Print("[")
+	for i, token := range tokens {
+		fmt.Print("'")
+		fmt.Print(token)
+		fmt.Print("'")
+		if i < len(tokens)-1 {
+			fmt.Println(", ")
+		}
+	}
+	fmt.Println("]")
+}
+
+
+func main() {
+	// parser := parser.MakeParser("1 a < a > | '\n' |")
+	// parser := parser.MakeParser("9 a < a > 1 + | '\n' |")
+	// parser := parser.MakeParser("{param1 < @ this < param1 > this > x , this >} cons < 2 cons > ! MyObject < MyObject > | '\n' |")
+	// parser := parser.MakeParser("7 b < b a < a > > | '\n' |")
+	// parser := parser.MakeParser("'# ' | $ input < 'you said: ' | input > | '\n' | ")
+	// parser := parser.MakeParser("1 'hello world!' 'your mom: ' | | '\n' | | '\n' |")
+
+	// parser := parser.MakeParser("{i < i > factorial < factorial > | ' factorial is: ' | {1 i > - i < i > factorial > * factorial <} {1 i > >>} & factorial > |} factorial < 7 factorial > ! '\n' |")
+	
+	// parser := parser.MakeParser("{condition < function < function > ! {function > !} {condition > !} &} dowhile < " +
+	// 														"{1 |} {0} dowhile > ! " +
+	// 														"'\n' |")
+	
+	// parser := parser.MakeParser("{condition < function < {function > ! {0} condition <} {condition > !} &} if < {1 |} {1} if > ! '\n' |")
+
+	// Making a game is possible using fox!
+	parser := parser.MakeParser("240 320 'Adam' make > ! 'It works!!!\n' |")
+	tokens := parser.Parse()
+	// PrintList(tokens)
+	VM := virtualmachine.MakeVM(tokens, library.GetLibrary())
+	library.InstallLibrary(&VM)
+	VM.Run()
+
+}
